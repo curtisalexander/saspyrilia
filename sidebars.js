060af3afec9184be7e682977b9bc9225ce9eb93f
Curtis Alexander <calex@calex.org>
2020-06-22 08:47:33 -0500

    ,

--- sidebars.js
+++ sidebars.js
@@ -11,7 +11,7 @@ module.exports = {
       type: 'category',
       label: 'Analysis',
       items: [
-        'analysis/filtering'
+        'analysis/filtering',
         'analysis/splitapplycombine',
       ]
     },
