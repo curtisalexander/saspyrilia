6b1525e2e0e912a54b004c84ba5d2f69b7504110
Curtis Alexander <calex@calex.org>
2020-07-09 08:13:58 -0500

    Derive columns

--- sidebars.js
+++ sidebars.js
@@ -11,7 +11,7 @@ module.exports = {
       type: 'category',
       label: 'Analysis',
       items: [
-        'analysis/derivevars',
+        'analysis/derivecolumns',
         'analysis/filtering',
         'analysis/selectcolumns',
         'analysis/splitapplycombine',
