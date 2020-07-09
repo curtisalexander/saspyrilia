3a97e7abe080cca70b724a557bf009078a4c522c
Curtis Alexander <calex@calex.org>
2020-07-09 08:12:52 -0500

    Add derivevars

--- sidebars.js
+++ sidebars.js
@@ -11,6 +11,7 @@ module.exports = {
       type: 'category',
       label: 'Analysis',
       items: [
+        'analysis/derivevars',
         'analysis/filtering',
         'analysis/selectcolumns',
         'analysis/splitapplycombine',
