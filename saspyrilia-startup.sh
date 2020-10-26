09ca5e8cb97a0a7a0cf8e77922a5eb74664260f5
Curtis Alexander <calex@calex.org>
2020-10-25 20:45:29 -0500

    add name

--- saspyrilia-startup.sh
+++ saspyrilia-startup.sh
@@ -3,16 +3,17 @@
 WORKDIR="app"
 
 # docker run
-docker run -d \
+docker run --rm \
+           --name saspyrilia
+           -d \
            -p 3000:3000 \
            -p 35729:35729 \
-           --mount "type=bind,src=${PWD}/blog,dst=/app"
-           -v $(pwd)/blog:/app/blog \
-           -v $(pwd)/docs:/app/docs \
-           -v $(pwd)/src:/app/src \
-           -v $(pwd)/static:/app/static \
-           -v $(pwd)/docusaurus.config.js:/app/docusaurus.config.js \
-           -v $(pwd)/sidebars.js:/app/sidebars.js \
+           --mount "type=bind,src=${PWD}/blog,dst=/app/blog" \
+           --mount "type=bind,src=${PWD}/docs,dst=/app/docs" \
+           --mount "type=bind,src=${PWD}/src,dst=/app/src" \
+           --mount "type=bind,src=${PWD}/static,dst=/app/static" \
+           --mount "type=bind,src=${PWD}/docusaurus.config.js,dst=/app/docusaurus.config.js" \
+           --mount "type=bind,src=${PWD}/sidebars.js,dst=/app/sidebars.js" \
            saspyrilia
 
 # pause so that the app can start
